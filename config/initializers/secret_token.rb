dcece762311ed5ea0535f1d4bdbf3f1fac0f79f8
Thomas Michiels <michiels.thomas@gmail.com>
2011-06-20 16:43:09 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Passionforhire::Application.config.secret_token = 'bffda92141a18f53dacddc9fcf4ca9c413f908001228de2c90936b5c91b196f27fcb06f24345e51069835cce7a2b0884d03db293c9af35e287ca0622222b3490'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Passionforhire::Application.config.session_store :cookie_store, :key => '_passionforhire_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Passionforhire::Application.config.session_store :active_record_store
